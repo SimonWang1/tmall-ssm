9516ef949ec0bbf5ada5efcd160d07b1077d689d
Wangsx <wangsixxx@sina.com>
2018-04-27 15:24:44 +0800

    update mybatis generator file

---
+++ src/main/java/com/wsx/tmall/mapper/PropertyValueMapper.java
@@ -0,0 +1,21 @@
+package com.wsx.tmall.mapper;
+
+import com.wsx.tmall.pojo.PropertyValue;
+import com.wsx.tmall.pojo.PropertyValueExample;
+import java.util.List;
+
+public interface PropertyValueMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PropertyValue record);
+
+    int insertSelective(PropertyValue record);
+
+    List<PropertyValue> selectByExample(PropertyValueExample example);
+
+    PropertyValue selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PropertyValue record);
+
+    int updateByPrimaryKey(PropertyValue record);
+}
